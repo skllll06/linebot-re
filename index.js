ea39baa9b932a02e2fabeed6a43391903415602e
kataoka <shunsuke.kataoka@shinkawa.co.jp>
2020-10-07 17:43:48 +0900

    add switch

--- index.js
+++ index.js
@@ -14,30 +14,48 @@ express()
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
-  .get("/g/", (req, res) => res.json({ method: "こんにちは、getさん" }))
-  .post("/p/", (req, res) => res.json({ method: "こんにちは、postさん" }))
   .post("/hook/", line.middleware(config), (req, res) => lineBot(req, res))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 function lineBot(req, res) {
+  //200番を返す
   res.status(200).end();
-  // ここから追加
+  // ボディからイベントを取得
   const events = req.body.events;
   const promises = [];
-  for (let i = 0, l = events.length; i < l; i++) {
+  for(let i=0;i<events.length;i++){
     const ev = events[i];
-    promises.push(
-      echoman(ev)
-    );
-  }
-  Promise.all(promises).then(console.log("pass"));
+    switch(ev.type){
+        case 'follow':
+            promises.push(greeting_follow(ev));
+            break;
+        case 'text':
+            promises.push(echoman(ev));
+            break;
+    }
 }
+Promise
+    .all(promises)
+    .then(console.log('all promises passed'))
+    .catch(e=>console.error(e.stack));
+}
+
 
 // 追加
 async function echoman(ev) {
-  const pro =  await client.getProfile(ev.source.userId);
+  //ユーザー名を取得
+  const pro = await client.getProfile(ev.source.userId);
+  //返事を送信
   return client.replyMessage(ev.replyToken, {
     type: "text",
     text: `${pro.displayName}さん、今「${ev.message.text}」って言いました？`
   })
+}
+
+const greeting_follow = async (ev) => {
+  const profile = await client.getProfile(ev.source.userId);
+  return client.replyMessage(ev.replyToken,{
+      "type":"text",
+      "text":`${profile.displayName}さん、フォローありがとうございます!\uDBC0\uDC04`
+  });
 }
