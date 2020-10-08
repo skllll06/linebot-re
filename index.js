da201a5a293535be1f6068e4f2ebe31e0e41e1b3
tennensui <sk10969.jp@i.softbank.jp>
2020-10-09 00:04:41 +0900

    delete

--- index.js
+++ index.js
@@ -25,62 +25,7 @@ const client = new line.Client(config);
 
 //リッチメニュー
 client.deleteDefaultRichMenu()
-const richmenu = {
-  "size": {
-    "width": 2500,
-    "height": 843
-  },
-  "selected": true,
-  "name": "リッチメニュー 1",
-  "chatBarText": "メニュー",
-  "areas": [
-    {
-      "bounds": {
-        "x": 0,
-        "y": 4,
-        "width": 825,
-        "height": 839
-      },
-      "action": {
-        "type": "message",
-        "text": "予約する"
-      }
-    },
-    {
-      "bounds": {
-        "x": 823,
-        "y": 0,
-        "width": 850,
-        "height": 839
-      },
-      "action": {
-        "type": "postback",
-        "data": "connect",
-      }
-    },
-    {
-      "bounds": {
-        "x": 1668,
-        "y": 0,
-        "width": 832,
-        "height": 839
-      },
-      "action": {
-        "type": "url",
-        "uri": "https://eeej.jp/villa_keisen/",
-        "altUri": {
-          "desktop": "https://eeej.jp/villa_keisen/"
-        }
-      }
-    }
-  ]
-}
-client.createRichMenu(richmenu)
-.then((richMenuId) => {
-  console.log(richMenuId)
-  console.log("0")
-  RichMenushow(richMenuId);
-})
+client.deleteRichMenu(richmenu-f4f8674aaf942ee45d980c54e7622f87)
 
 //テーブル作成(userテーブル)
 const create_userTable =
