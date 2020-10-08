e28997f71afa49cab68ddb652cedfbb8ba5d31bd
tennensui <sk10969.jp@i.softbank.jp>
2020-10-08 21:27:11 +0900

    test

--- index.js
+++ index.js
@@ -102,12 +102,12 @@ function lineBot(req, res) {
   client.createRichMenu(richmenu)
   .then((richMenuId) =>
     console.log(richMenuId))
-    client.setRichMenuImage(richMenuId, fs.createReadStream('./images/richmenu_def.jpg'))
-    client.getRichMenu(richMenuId)
-    .then((richMenu) => {
-      console.log(richMenu.size);
-      console.log(richMenu.areas[0].bounds);
-    })
+    // client.setRichMenuImage(richMenuId, fs.createReadStream('./images/richmenu_def.jpg'))
+    // client.getRichMenu(richMenuId)
+    // .then((richMenu) => {
+    //   console.log(richMenu.size);
+    //   console.log(richMenu.areas[0].bounds);
+    // })
   const events = req.body.events;
   const promises = [];
   for (let i = 0; i < events.length; i++) {
